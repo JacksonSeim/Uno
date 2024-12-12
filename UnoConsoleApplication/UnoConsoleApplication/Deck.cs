32c8aa0260cf6dad95b16b5beebdcb6c53967140
JacksonSeim <jseim4699@gmail.com>
2024-12-12 09:41:54 -0600

    added the expected classes for this project

---
+++ UnoConsoleApplication/UnoConsoleApplication/Deck.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnoConsoleApplication
+{
+    public class Deck : ICollection<Card>
+    {
+        public List<Card> _deck = new List<Card>();
+
+        #region ICollectionMethods
+        public int Count => ((ICollection<Card>)_deck).Count;
+
+        public bool IsReadOnly => ((ICollection<Card>)_deck).IsReadOnly;
+
+        public void Add(Card item)
+        {
+            ((ICollection<Card>)_deck).Add(item);
+        }
+
+        public void Clear()
+        {
+            ((ICollection<Card>)_deck).Clear();
+        }
+
+        public bool Contains(Card item)
+        {
+            return ((ICollection<Card>)_deck).Contains(item);
+        }
+
+        public void CopyTo(Card[] array, int arrayIndex)
+        {
+            ((ICollection<Card>)_deck).CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<Card> GetEnumerator()
+        {
+            return ((IEnumerable<Card>)_deck).GetEnumerator();
+        }
+
+        public bool Remove(Card item)
+        {
+            return ((ICollection<Card>)_deck).Remove(item);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)_deck).GetEnumerator();
+        }
+        #endregion
+
+
+    }
+}
---
+++ UnoConsoleApplication/UnoConsoleApplication/DiscardPile.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnoConsoleApplication
+{
+    public class DiscardPile : ICollection<Card>
+    {
+        public List<Card> _discardPile = new List<Card>();
+
+        #region ICollectionMethods
+        public int Count => ((ICollection<Card>)_discardPile).Count;
+
+        public bool IsReadOnly => ((ICollection<Card>)_discardPile).IsReadOnly;
+
+        public void Add(Card item)
+        {
+            ((ICollection<Card>)_discardPile).Add(item);
+        }
+
+        public void Clear()
+        {
+            ((ICollection<Card>)_discardPile).Clear();
+        }
+
+        public bool Contains(Card item)
+        {
+            return ((ICollection<Card>)_discardPile).Contains(item);
+        }
+
+        public void CopyTo(Card[] array, int arrayIndex)
+        {
+            ((ICollection<Card>)_discardPile).CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<Card> GetEnumerator()
+        {
+            return ((IEnumerable<Card>)_discardPile).GetEnumerator();
+        }
+
+        public bool Remove(Card item)
+        {
+            return ((ICollection<Card>)_discardPile).Remove(item);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)_discardPile).GetEnumerator();
+        }
+        #endregion
+    }
+}
